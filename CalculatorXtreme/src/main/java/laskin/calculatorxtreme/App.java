c892ba7ac31d5bfa4403d9e0dae6a5a537a7adbb
Haarala Akseli M <akselhaa@da1-cs-bk107-23.helsinki.fi>
2014-09-04 12:46:30 +0300

    javadoc generoitu

--- CalculatorXtreme/src/main/java/laskin/calculatorxtreme/App.java
+++ CalculatorXtreme/src/main/java/laskin/calculatorxtreme/App.java
@@ -1,11 +1,7 @@
 package laskin.calculatorxtreme;
 
 
-import java.util.Scanner;
 import laskin.calculatorxtreme.sovelluslogiikka.kirjasto.*;
-import laskin.calculatorxtreme.kayttoliittyma.TekstiKayttoliittyma;
-import laskin.calculatorxtreme.sovelluslogiikka.lausekelogiikka.Lohko;
-import laskin.calculatorxtreme.sovelluslogiikka.lausekelogiikka.Luku;
 import javax.swing.SwingUtilities;
 import laskin.calculatorxtreme.kayttoliittyma.GraafinenKayttoliittyma;
 
@@ -14,15 +10,8 @@ public class App {
     public static void main( String[] args ) {                
         
         ToimintoKirjasto kirjasto = new ToimintoKirjasto();
-//        
-//        Scanner lukija = new Scanner(System.in);
-//        
-//        TekstiKayttoliittyma  kayttoliittyma 
-//                = new TekstiKayttoliittyma(lukija, kirjasto);
-//        
-//        kayttoliittyma.kaynnista();
-//        
         GraafinenKayttoliittyma kayttoliittyma = new GraafinenKayttoliittyma(kirjasto);
+        
         SwingUtilities.invokeLater(kayttoliittyma);
     }
 }
